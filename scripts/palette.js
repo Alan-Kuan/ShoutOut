2c2a3b787a19f4d0b07b71058c8b63371ece9694
Alan-Kuan <kuanhaoan@gmail.com>
2022-07-02 20:48:59 +0800

    feat: able to pick a theme color.

--- scripts/palette.js
+++ scripts/palette.js
@@ -12,3 +12,21 @@ document.querySelector('#palette-btn').addEventListener('click', () => {
         palette.classList.remove('slide-out');
     }
 });
+
+document.querySelectorAll('.color').forEach(item => {
+    item.addEventListener('click', e => {
+        let color_block = e.target;
+        if (color_block.classList.contains('selected'))
+            return;
+
+        let color = window.getComputedStyle(color_block).getPropertyValue('background-color');
+        document.querySelector(':root').style.setProperty('--theme-color', color);
+
+        document.querySelectorAll('.color').forEach(block => {
+            if (!block.classList.contains('selected'))
+                return;
+            block.classList.remove('selected');
+        });
+        color_block.classList.add('selected');
+    });
+});
