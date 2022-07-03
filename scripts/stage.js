2832503dc1336a8a984b8c2aada1d9cc9a41aedb
Alan-Kuan <kuanhaoan@gmail.com>
2022-07-03 14:27:43 +0800

    feat: hide cursor if it's idling on stage.

--- scripts/stage.js
+++ scripts/stage.js
@@ -2,18 +2,6 @@
     Maximize and unmaximize the stage.
 */
 
-document.querySelector('#close-btn').addEventListener('click', () => {  
-    maximizeStage();
-});
-
-document.querySelector('#show-panel-btn').addEventListener('click', () => { 
-    unmaximizeStage();
-});
-
-document.querySelector('#stage').addEventListener('touchend', () => { 
-    unmaximizeStage();
-});
-
 function maximizeStage() {
     slideOut('#panel-container');
     closeAllCards();
@@ -26,3 +14,39 @@ function unmaximizeStage() {
     document.querySelector('#stage').classList.remove('maximized');
     document.querySelector('#show-panel-btn').classList.remove('show');
 }
+
+document.querySelector('#close-btn').addEventListener('click', () => {  
+    maximizeStage();
+});
+
+document.querySelector('#show-panel-btn').addEventListener('click', () => { 
+    unmaximizeStage();
+});
+
+document.querySelector('#stage').addEventListener('touchend', () => { 
+    unmaximizeStage();
+});
+
+/*
+    Hide Idling Cursor
+*/
+
+document.querySelector('#stage').addEventListener('mousemove', e => {
+    let stage = e.target;
+
+    // clear timer
+    const old_timer = stage.getAttribute('timer');
+    if (old_timer) {
+        clearTimeout(old_timer);
+        stage.setAttribute('timer', '');
+    }
+
+    // set timer
+    const timer = setTimeout(() => { 
+        stage.setAttribute('timer', '');
+        stage.style.cursor = 'none';
+    }, 3500);
+    stage.setAttribute('timer', timer);
+
+    stage.style.cursor = 'default';
+});
