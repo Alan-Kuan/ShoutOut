6cd65a422f8db38f6aa6bf522ab9a180e504c2ea
Alan-Kuan <kuanhaoan@gmail.com>
2022-07-03 09:29:54 +0800

    fix: prevent from sending empty string.

--- scripts/msg-form.js
+++ scripts/msg-form.js
@@ -5,6 +5,8 @@
 document.querySelector('#msg-form').addEventListener('submit', e => { 
     e.preventDefault();
     let msg_text = document.querySelector('#msg-form .text-field').value;
+    if (msg_text.length === 0)
+        return;
     startMsg(msg_text);
 });
 
